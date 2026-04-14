610c0627fa45061fa857ef792776a4719622d76b
Namatullah <namatullah.shahbazi444@gmail.com>
2026-04-14 20:53:36 +0430

    post initial add

--- lib/pages/post.dart
+++ lib/pages/post.dart
@@ -1,14 +1,85 @@
 import 'package:flutter/material.dart';
 
-class Post extends StatelessWidget {
+class Post extends StatefulWidget {
   const Post({super.key});
+
+  @override
+  State<Post> createState() => _PostState();
+}
+
+class _PostState extends State<Post> {
+  final List<Map<String, String>> posts = [];
+  void _addPost(String title, String description) {
+    setState(() {
+      posts.add({'title': title, 'description': description});
+    });
+  }
+
+  void _showAddPostDialog() {
+    final titleController = TextEditingController();
+    final descriptionController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: const Text('Add Post'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: titleController,
+                decoration: const InputDecoration(labelText: 'Title'),
+              ),
+              SizedBox(height: 15),
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(labelText: 'Description'),
+              ),
+            ],
+          ),
+
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: const Text('Cancel'),
+            ),
+
+            TextButton(
+              onPressed: () {
+                _addPost(titleController.text, descriptionController.text);
+                Navigator.pop(context);
+              },
+              child: const Text('Add'),
+            ),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Post")),
-      body: const Center(
-        child: Text("Post Page", style: TextStyle(fontSize: 20)),
+      appBar: AppBar(title: const Text('Posts')),
+      body: posts.isEmpty
+          ? const Center(child: Text('No posts yet'))
+          : ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final post = posts[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    title: Text(post['title'] ?? ''),
+                    subtitle: Text(post['description'] ?? ''),
+                  ),
+                );
+              },
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _showAddPostDialog,
+        child: const Icon(Icons.add),
       ),
     );
   }
